c609807a783e39b6fc4fa128b95222e4dc32d398
春风亭茅十八 <64020717+hakunaMatata-haus@users.noreply.github.com>
2023-03-09 10:46:25 +0800

    Update counter.sol

--- w1-1/counter.sol
+++ w1-1/counter.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
+contract Counter{
+    uint256 public test_num = 1;
+
+    function add(uint256 num) internal pure returns(uint256 res){
+         res = num + 1;
+    }
+    
+    function useAdd() external{
+        test_num = add(test_num);
+    }
+
+}
